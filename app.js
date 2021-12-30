4a9ca6b8750fe72518beb7bcabc5342b41b48968
jit <jitbanerjee4@gmail.com>
2021-12-30 20:22:34 +0530

    Update 1

--- app.js
+++ app.js
@@ -181,7 +181,8 @@ app.use((err, req, res, next)=>{
     res.status(statusCode).render('error', {err});
 })
 
+const port= 5000 || process.env.PORT;
 
-app.listen(5000, () =>{
-    console.log("Server started at 5000");
+app.listen(port, () =>{
+    console.log(`Server started at ${port}`);
 })

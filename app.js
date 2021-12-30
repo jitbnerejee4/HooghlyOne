02c1ece87e472be55823b443ee29997b2588f052
jit <jitbanerjee4@gmail.com>
2021-12-30 20:54:05 +0530

    Update 2

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ const Visitor= require('./models/Visitor');
 const MongoStore = require('connect-mongo');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require("helmet");
-const db_url= process.env.DB_URL || 'mongodb://localhost:27017/eTreasury';
+const db_url= process.env.DB_URL;
 
 const app = express();
 
-
+//'mongodb://localhost:27017/eTreasury'
 mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() =>{
         console.log("CONNECTION OPEN")
